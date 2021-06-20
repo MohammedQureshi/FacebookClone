b51d1d94d6f3bd727ec577ff6b448b406f4172fe
Mohammed Faisal Qureshi <mohammedqureshi2998@gmail.com>
2021-06-20 16:43:23 +0100

    Format

--- src/MiddlePanel/MiddlePanel.js
+++ src/MiddlePanel/MiddlePanel.js
@@ -1,37 +1,37 @@
-import React , {useState, useEffect} from 'react'
-import Post from '../Post/Post';
-import './MiddlePanel.css'
-import db from '../firebase'
-import PostTextBox from './PostTextBox'
+import React, { useState, useEffect } from "react";
+import Post from "../Post/Post";
+import "./MiddlePanel.css";
+import db from "../firebase";
+import PostTextBox from "./PostTextBox";
 
 function MiddlePanel() {
-    const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
 
-    useEffect(() => {
-        db.collection('posts').onSnapshot(snapshot => (
-            setPosts(snapshot.docs.map(doc => doc.data()))
-        ))
-    }, [])
-    return (
-      <div className="MiddlePanel">
-          <div>
-              <PostTextBox />
-          </div>
-          <div>
-            {posts.map(post => (
-                <Post 
-                name={post.name}
-                avatar={post.avatar}
-                time={post.time}
-                audience={post.audience}
-                text={post.text}
-                content={post.content}
-                verified={post.verified}
-                />
-            ))}
-          </div>
-      </div>
+  useEffect(() => {
+    db.collection("posts").onSnapshot((snapshot) =>
+      setPosts(snapshot.docs.map((doc) => doc.data()))
     );
+  }, []);
+  return (
+    <div className="MiddlePanel">
+      <div>
+        <PostTextBox />
+      </div>
+      <div>
+        {posts.map((post) => (
+          <Post
+            name={post.name}
+            avatar={post.avatar}
+            time={post.time}
+            audience={post.audience}
+            text={post.text}
+            content={post.content}
+            verified={post.verified}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default MiddlePanel
+export default MiddlePanel;
--- src/MiddlePanel/PopOutPost.js
+++ src/MiddlePanel/PopOutPost.js
@@ -1,36 +1,40 @@
-import React from 'react'
-import './PopOutPost.css'
-import Avatar from '@material-ui/core/Avatar'
+import React from "react";
+import "./PopOutPost.css";
+import Avatar from "@material-ui/core/Avatar";
 
-function PopOutPost({toggleCollapse}) {
-    return (
-        <div className="PopOutPost">
-            <div className="PopOutPost__Post">
-                <div className="PopOutPost__Title">
-                    <h3> Create post </h3> 
-                    <button onClick={() => { toggleCollapse()}} > X </button>
-                </div>
-                <div className="PopOutPost__Header">
-                    <div className="PopOutPost__Avatar">
-                        <Avatar src="https://media-exp3.licdn.com/dms/image/C4E03AQF2s8dGWpgC5g/profile-displayphoto-shrink_200_200/0/1592998404125?e=1629331200&v=beta&t=5IRMkWHeb6gNJBhmuZZcLKYNGjyaJ6Pd3F-ER5lVRxY" />
-                    </div>
-                    <div className="PopOutPost__Name">
-                        <h3>Mohammed Qureshi</h3>
-                        <select>
-                            <option>Public</option>
-                            <option>Friends</option>
-                        </select>
-                    </div>
-                </div>
-                <div className="PopOutPost__InputBox">
-                    <textarea rows="8" cols="50" placeholder="What's on your mind, Mohammed?"></textarea>
-                </div>
-                <div className="PopOutPost__Submit">
-                    <button>Post</button>
-                </div>
-            </div>
+function PopOutPost({ toggleCollapse }) {
+  return (
+    <div className="PopOutPost">
+      <div className="PopOutPost__Post">
+        <div className="PopOutPost__Title">
+          <h3> Create post </h3>
+          <button onClick={() => toggleCollapse()}> X </button>
         </div>
-    )
+        <div className="PopOutPost__Header">
+          <div className="PopOutPost__Avatar">
+            <Avatar src="https://media-exp3.licdn.com/dms/image/C4E03AQF2s8dGWpgC5g/profile-displayphoto-shrink_200_200/0/1592998404125?e=1629331200&v=beta&t=5IRMkWHeb6gNJBhmuZZcLKYNGjyaJ6Pd3F-ER5lVRxY" />
+          </div>
+          <div className="PopOutPost__Name">
+            <h3>Mohammed Qureshi</h3>
+            <select>
+              <option>Public</option>
+              <option>Friends</option>
+            </select>
+          </div>
+        </div>
+        <div className="PopOutPost__InputBox">
+          <textarea
+            rows="8"
+            cols="50"
+            placeholder="What's on your mind, Mohammed?"
+          ></textarea>
+        </div>
+        <div className="PopOutPost__Submit">
+          <button>Post</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default PopOutPost
+export default PopOutPost;
